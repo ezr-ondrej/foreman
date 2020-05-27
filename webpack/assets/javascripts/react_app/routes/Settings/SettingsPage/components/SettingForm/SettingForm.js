d8e784ed9bb2318a205c8203e842053d33f08dd5
Ondřej Ezr <oezr@redhat.com>
2020-05-27 11:35:04 +0200

    Use the FormField properly

--- webpack/assets/javascripts/react_app/routes/Settings/SettingsPage/components/SettingForm/SettingForm.js
+++ webpack/assets/javascripts/react_app/routes/Settings/SettingsPage/components/SettingForm/SettingForm.js
@@ -3,10 +3,28 @@ import PropTypes from 'prop-types';
 import { Field as FormikField } from 'formik';
 
 import ForemanForm from '../../../../../components/common/forms/ForemanForm';
-import SettingValueField from './SettingValueField';
+import FormField from '../../../../../components/common/forms/FormField';
 import { SETTING_UPDATE_PATH } from '../../../constants';
 
 import { translate as __ } from '../../../../../common/I18n';
+
+const getSettingProps = settingModel => {
+  let options = {};
+  if ( settingModel.settingsType === 'boolean' ) return {
+    options: { true: 'Yes', false: 'No'},
+    type: 'select',
+    useSelect2: false,
+  };
+  if ( !settingModel.selectValues ) return {};
+  switch (settingModel.selectValues.kind) {
+    case 'array':
+    case 'hash':
+      options = settingModel.selectValues.collection;
+      return { options, type: 'select', useSelect2: false }
+    default:
+      return {};
+  }
+};
 
 const SettingForm = props => {
   const handleSubmit = async (values, actions) => {
@@ -33,11 +51,11 @@ const SettingForm = props => {
       initialValues={props.initialValues}
       onCancel={props.setModalClosed}
     >
-      <FormikField
-        name="value"
-        component={SettingValueField}
-        setting={props.setting}
-      />
+      <FormikField name="value" >
+        {({ field, form: {errors}, meta }) => (
+          <FormField {...field} error={errors.value} type={getSettingProps(props.setting).type} inputProps={getSettingProps(props.setting)} />
+        )}
+      </FormikField>
     </ForemanForm>
   );
 };
